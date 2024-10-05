26f3071105e16c46c5223f0fc317612790b20290
Developer1 <loukik.khound@gmail.com>
2024-10-06 01:05:16 +0530

    dashbTest updated

--- myProject/src/test/java/com/finalcut/dashbTest.java
+++ myProject/src/test/java/com/finalcut/dashbTest.java
@@ -11,9 +11,9 @@ public class dashbTest {
 		dashb d1=new dashb();
 		assertEquals("You Entered: aaa and bbb",d1.add("aaa", "bbb"));
 		dashb d2=new dashb();
-		assertEquals("You Entered: aaa and bbb",d2.add("aaa", "bbb"));
+		assertEquals("You Entered: 123 and 234",d2.add("123", "234"));
 		dashb d3=new dashb();
-		assertEquals("You Entered: aaa and bbb",d3.add("aaa", "bbb"));
+		assertEquals("You Entered: a2b and a@n",d3.add("a2b", "a@n"));
 	}
 
 }
