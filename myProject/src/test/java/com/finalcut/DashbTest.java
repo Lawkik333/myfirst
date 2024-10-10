04f1aab4e262e04c849c133b53c45197c0f23dc0
Developer1 <loukik.khound@gmail.com>
2024-10-10 23:59:56 +0530

    correcting java release version

--- myProject/src/test/java/com/finalcut/DashbTest.java
+++ myProject/src/test/java/com/finalcut/DashbTest.java
@@ -4,15 +4,15 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class dashbTest {
+public class DashbTest {
 
 	@Test
 	public void test() {
-		dashb d1=new dashb();
+		Dashb d1=new Dashb();
 		assertEquals("You Entered: aaa and bbb",d1.add("aaa", "bbb"));
-		dashb d2=new dashb();
+		Dashb d2=new Dashb();
 		assertEquals("You Entered: 123 and 234",d2.add("123", "234"));
-		dashb d3=new dashb();
+		Dashb d3=new Dashb();
 		assertEquals("You Entered: a2b and a@n",d3.add("a2b", "a@n"));
 	}
 
